9e6f7f2f8c014d0238e9e9553d3c53f404ffc158
J. Andrew Casey-Clyde <andrew.casey-clyde@uconn.edu>
2021-02-08 19:46:04 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Multimessenger interpretations of the nanoHertz gravitational wave background.',
+    author='J. Andrew Casey-Clyde',
+    license='MIT',
+)
